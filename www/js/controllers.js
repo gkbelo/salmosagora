4e6477450c36bfa691d9a85040acddd0f1c3f1fc
gkbelo <gkbelo@gmail.com>
2017-03-23 23:35:06 -0300

    compartilhar via imagem ok

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,32 +38,11 @@ angular.module('psalmstoday')
       $scope.psalm = dados.psalm[$stateParams.psalmId-1];
     });
     
-    $scope.theText = 'share this';
-/*    $scope.toShare = function() {
-      window.plugins.socialsharing.share($scope.theText);
-    };
-
     $scope.takeScreenshot = function() {
       navigator.screenshot.save((function(e, r) {
-        if (e) {
-          $ionicPopup.alert({ title: 'Error!'
-                            , template: 'Screenshot unsuccessful' });
-        } else {
-          $ionicPopup.confirm({ title: 'Screenshot successful'
-                              , template: 'Click OK to share'
-          }).then(function(res) {
-            if (res) {
-              window.plugins.socialsharing.share(null, null, 'file://' + r.filePath);
-            } else {
-              $ionicPopup.alert({ title: 'Error!'
-                                , template: 'Could not share'
-              });
-            }
-          });
-        }
-      }), 'jpg', 50, 'randomScreenshot');
+        window.plugins.socialsharing.share(null, null, 'file://' + r.filePath);
+      }), 'jpg', 100, 'myPsalm');
     };
-    */
 
     //slider_functions
     $scope.sliderOptions = {
